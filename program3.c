970b1f5d1460b52c432758f40d65e72fe8e3f003
september-a <september.abbott@gmail.com>
2024-04-10 13:34:33 -0500

    changed to use stdin

--- program3.c
+++ program3.c
@@ -3,43 +3,50 @@
 
 #define BUFFER_SIZE 1024
 
-// Function to check if a file is text or binary
 int is_text_file(FILE *file);
 
-int main(int argc, char *argv[]) {
+void convert_to_unix(FILE *file);
+
+int main() {
     char filename[BUFFER_SIZE];
     FILE *input_file;
-    int i;
+
+    // Flag to indicate if the "-u" flag has been encountered
+    int unix_conversion_flag = 0;
 
     // Loop through each filename from stdin
-    for (i = 1; i < argc; i++) {
-        // if the argument is a string - ignore (for now) and move on
-        if (strcmp(argv[i], "-u") == 0){
+    while (fgets(filename, BUFFER_SIZE, stdin) != NULL) {
+        // Remove newline character from the end of filename
+        filename[strcspn(filename, "\n")] = '\0';
+
+        if (strcmp(filename, "-u") == 0) {
+            unix_conversion_flag = 1;
             continue;
         }
+
         // Open the file
-        input_file = fopen(argv[i], "rb");
+        input_file = fopen(filename, "rb");
         if (input_file == NULL) {
             // Error message if file could not be opened
-            fprintf(stderr, "Error: Could not open file %s\n", argv[i]);
+            fprintf(stderr, "Error: Could not open file %s\n", filename);
             continue;
         }
 
         // Check if the file is a text file
         if (!is_text_file(input_file)) {
             // Display message on stderr if file is not text
-            fprintf(stderr, "%s is not a text file\n", argv[i]);
+            fprintf(stderr, "%s is not a text file\n", filename);
             fclose(input_file);
             continue;
         }
 
         // Display the filename on stdout
-        printf("%s\n", argv[i]);
+        printf("%s is a txt file\n", filename);
 
         // If "-u" flag is provided and file is Windows formatted text
-        if (argc > 2 && strcmp(argv[1], "-u") == 0) {
-            // convert(input_file) to be implemented
-            printf(" to be converted to unix");
+        if (unix_conversion_flag) {
+            convert_to_unix(input_file);
+            unix_conversion_flag = 0; // Reset the flag after conversion
         }
 
         fclose(input_file);
@@ -52,13 +59,29 @@ int main(int argc, char *argv[]) {
 int is_text_file(FILE *file) {
     int ch;
     while ((ch = fgetc(file)) != EOF) {
-        if (!(ch == '\n' || ch == '\r' || (ch >= 32 && ch <= 126))) {
+        if (!(ch == '\n' || (ch >= 32 && ch <= 126))) {
             rewind(file);
-            printf("This is not a txt file : ");
             return 0; // Not a text file
         }
     }
     rewind(file);
-    printf("This is a txt file : ");
     return 1; // Text file
 }
+
+// Function to convert Windows formatted text file to Unix format
+void convert_to_unix(FILE *file) {
+    FILE *temp_file = tmpfile();
+    int ch;
+    while ((ch = fgetc(file)) != EOF) {
+        if (ch != '\r') {
+            fputc(ch, temp_file);
+        }
+    }
+    rewind(temp_file);
+    rewind(file);
+    while ((ch = fgetc(temp_file)) != EOF) {
+        fputc(ch, file);
+    }
+    fclose(temp_file);
+}
+
